eae818f27b1d0c0977487a17bb19d39d083a856b
Essien Nathaniel <thaniessy@gmail.com>
2022-11-19 15:07:05 +0100

    Counter components updated

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,33 @@
-import React, { useReducer } from 'react'
+import React, { useReducer } from 'react';
+import { Button } fom 'react-bootstrap';
+
+//Counter App using useReducer to manage the states
 
 const Counter = () => {
 	const [state, dispatch] = useReducer(reducer, initialState)
-	const initialState = { count: 0 }
-	function reducer (state, action){
-	Switch(actiontype){
-	Case "increment":
-	return{count: state.count + 1}
-	Case "decrement":
-	return{count: state.count  1}
-	Case "reset":
-	return initialState
-	default:
-	return initialState
-	}
+	const initialState = {
+		count: 0
 	}
 
-  return (
-    <div>Counter</div>
-  )
+	function reducer(state, action) {
+		switch (action.type) {
+			case "increment":
+				return { count: state.count + 1 }
+			case "decrement":
+				return { count: state.count - 1 }
+			case "reset":
+				return initialState
+			default:
+				return initialState
+		}
+	}
+
+	return (
+		<div><h1>Count : {state} </h1> <br />
+			<Button variant='danger' onClick={() => dispatch({ type: 'decrement' })}>Decrement</Button> <br />
+			<Button variant='primary' onClick={() => dispatch({ type: 'reset' })}>Reset</Button> <br />
+			<Button variant='success' onClick={() => dispatch({ type: 'increment' })}>Increment</Button></div>
+	)
 }
 
 export default Counter
